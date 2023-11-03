773166aab9179d6d0f556287f37c6d8595e35746
Salma-Engmelb <docmel2409@gmail.com>
2023-11-03 11:51:31 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,12 +17,14 @@ char *create_buffer(char *file)
 
 	buffer = malloc(sizeof(char) * 1024);
 
-	/*if (buffer == NULL)
+	/**
+	 * if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
 			"Error: Can't write to %s\n", file);
 		exit(99);
-	}*/
+	}
+	*/
 
 	return (buffer);
 }
@@ -37,11 +39,13 @@ void close_file(int fd)
 
 	c = close(fd);
 
-	/*if (c == -1)
+	/**
+	 * if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
-	}*/
+	}
+	*/
 }
 
 /**
@@ -61,11 +65,13 @@ int main(int argc, char *argv[])
 	int from, to, r, w;
 	char *buffer;
 
-	/*if (argc != 3)
+	/**
+	 * if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-	}*/
+	}
+	*/
 
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
@@ -73,22 +79,26 @@ int main(int argc, char *argv[])
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		/*if (from == -1 || r == -1)
+		/**
+		 * if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
-		}*/
+		}
+		*/
 
 		w = write(to, buffer, r);
-		/*if (to == -1 || w == -1)
+		/**
+		 * if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
-		}*/
+		}
+		*/
 
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
